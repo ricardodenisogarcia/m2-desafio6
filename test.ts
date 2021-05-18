ba55848bf34bc7b6d6ebd4db850f9e119f15f73c
Ricardo <richard.mdq@gmail.com>
2021-05-18 11:21:33 +0100

    2 Test passed

--- test.ts
+++ test.ts
@@ -1,4 +1,17 @@
 import test from "ava";
+import { User, Product } from "./index";
 
-test("Testeo la clase User", (t) => {});
-test("Testeo la clase Product y sus métodos", (t) => {});
+test("Testeo la clase User", (t) => {
+  var usuario1 = new User("Roque");
+  t.is(usuario1.name, "Roque");
+});
+test("Testeo la clase Product y sus métodos", (t) => {
+  var producto1 = new Product("Teclado", 25);
+  t.is(producto1.name, "Teclado");
+  t.is(producto1.price, 25);
+  var usuario2 = new User("Mikel");
+  var producto2 = new Product("Monitor", 125);
+  usuario2.addProduct(producto2);
+  t.is(usuario2.products[0].name, "Monitor");
+  t.is(usuario2.products[0].price, 125);
+});
